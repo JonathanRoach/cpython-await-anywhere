5af1db2f8c169ec56c87b4aec412089d807082aa
JonathanRoach <jonroach@icloud.com>
2026-01-12 10:57:50 +0000

    Expand varieties of ENSURE_STACK macros

--- Include/internal/pycore_cor_tools.h
+++ Include/internal/pycore_cor_tools.h
@@ -335,6 +335,102 @@ static void *Do_##fn(void *_params) { \
     p2_t p2 = params->v2;
 
 
+#define _PY_ENSURE_STACK_FOR_FN4_A(fn, p0_t, p1_t, p2_t, p3_t) \
+static void *Do_##fn(void *); \
+struct Do_Call_Params_##fn { \
+    p0_t v0; \
+    p1_t v1; \
+    p2_t v2; \
+    p3_t v3; \
+};
+// Function definition start goes here
+#define _PY_ENSURE_STACK_FOR_FN4_B(space, nomemret, r_t, fn, p0_t, p0, p1_t, p1, p2_t, p2, p3_t, p3) \
+    struct Do_Call_Params_##fn params = {p0, p1, p2, p3}; \
+    if (_Py_Coroutine_GetStackHeadroom() < (intptr_t)(space) ){ \
+        void *result; \
+        if ( !_Py_Coroutine_Chain(space > PYOS_COSTACK_STD_SIZE ? space : PYOS_COSTACK_STD_SIZE, Do_##fn, &params, &result)) { \
+            return (r_t)result; \
+        } else { \
+            PyErr_NoMemory(); \
+            return nomemret; \
+        } \
+    } \
+    return (r_t)(intptr_t)Do_##fn(&params); \
+} \
+static void *Do_##fn(void *_params) { \
+    struct Do_Call_Params_##fn *params = (struct Do_Call_Params_##fn *)_params; \
+    p0_t p0 = params->v0; \
+    p1_t p1 = params->v1; \
+    p2_t p2 = params->v2; \
+    p3_t p3 = params->v3;
+
+
+#define _PY_ENSURE_STACK_FOR_FN5_A(fn, p0_t, p1_t, p2_t, p3_t, p4_t) \
+static void *Do_##fn(void *); \
+struct Do_Call_Params_##fn { \
+    p0_t v0; \
+    p1_t v1; \
+    p2_t v2; \
+    p3_t v3; \
+    p4_t v4; \
+};
+// Function definition start goes here
+#define _PY_ENSURE_STACK_FOR_FN5_B(space, nomemret, r_t, fn, p0_t, p0, p1_t, p1, p2_t, p2, p3_t, p3, p4_t, p4) \
+    struct Do_Call_Params_##fn params = {p0, p1, p2, p3, p4}; \
+    if (_Py_Coroutine_GetStackHeadroom() < (intptr_t)(space) ){ \
+        void *result; \
+        if ( !_Py_Coroutine_Chain(space > PYOS_COSTACK_STD_SIZE ? space : PYOS_COSTACK_STD_SIZE, Do_##fn, &params, &result)) { \
+            return (r_t)result; \
+        } else { \
+            PyErr_NoMemory(); \
+            return nomemret; \
+        } \
+    } \
+    return (r_t)(intptr_t)Do_##fn(&params); \
+} \
+static void *Do_##fn(void *_params) { \
+    struct Do_Call_Params_##fn *params = (struct Do_Call_Params_##fn *)_params; \
+    p0_t p0 = params->v0; \
+    p1_t p1 = params->v1; \
+    p2_t p2 = params->v2; \
+    p3_t p3 = params->v3; \
+    p4_t p4 = params->v4;
+
+
+#define _PY_ENSURE_STACK_FOR_FN6_A(fn, p0_t, p1_t, p2_t, p3_t, p4_t, p5_t) \
+static void *Do_##fn(void *); \
+struct Do_Call_Params_##fn { \
+    p0_t v0; \
+    p1_t v1; \
+    p2_t v2; \
+    p3_t v3; \
+    p4_t v4; \
+    p5_t v5; \
+};
+// Function definition start goes here
+#define _PY_ENSURE_STACK_FOR_FN6_B(space, nomemret, r_t, fn, p0_t, p0, p1_t, p1, p2_t, p2, p3_t, p3, p4_t, p4, p5_t, p5) \
+    struct Do_Call_Params_##fn params = {p0, p1, p2, p3, p4, p5}; \
+    if (_Py_Coroutine_GetStackHeadroom() < (intptr_t)(space) ){ \
+        void *result; \
+        if ( !_Py_Coroutine_Chain(space > PYOS_COSTACK_STD_SIZE ? space : PYOS_COSTACK_STD_SIZE, Do_##fn, &params, &result)) { \
+            return (r_t)result; \
+        } else { \
+            PyErr_NoMemory(); \
+            return nomemret; \
+        } \
+    } \
+    return (r_t)(intptr_t)Do_##fn(&params); \
+} \
+static void *Do_##fn(void *_params) { \
+    struct Do_Call_Params_##fn *params = (struct Do_Call_Params_##fn *)_params; \
+    p0_t p0 = params->v0; \
+    p1_t p1 = params->v1; \
+    p2_t p2 = params->v2; \
+    p3_t p3 = params->v3; \
+    p4_t p4 = params->v4; \
+    p5_t p5 = params->v5;
+
+
 #define _PY_ENSURE_STACK_FOR_FN7_A(fn, p0_t, p1_t, p2_t, p3_t, p4_t, p5_t, p6_t) \
 static void *Do_##fn(void *); \
 struct Do_Call_Params_##fn { \
