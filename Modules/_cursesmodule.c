f5ab572fbe08a463eb5f504a0653b46f4f2ab247
JonathanRoach <jonroach@icloud.com>
2026-03-16 12:31:26 +0000

    Fix silly mistake

--- Modules/_cursesmodule.c
+++ Modules/_cursesmodule.c
@@ -3757,7 +3757,7 @@ _curses_init_pair_impl(PyObject *module, int pair_number, int fg, int bg)
     Py_RETURN_NONE;
 }
 
-_PY_ENSURE_STACK_FOR_FN3_A(PyObject *, _curses_initscr_impl, PyObject *)
+_PY_ENSURE_STACK_FOR_FN1_A(PyObject *, _curses_initscr_impl, PyObject *)
 /*[clinic input]
 @c_stack_frugal
 _curses.initscr
@@ -3772,7 +3772,7 @@ _curses_initscr_impl(PyObject *module)
 /*[clinic end generated code: output=619fb68443810b7b input=041d3d675ca935d3]*/
 {
     // seen using 103808 bytes on Fedora 43 x86 64 bit
-    _PY_ENSURE_STACK_FOR_FN3_B(128*1024, NULL, PyObject *, _curses_initscr_impl, PyObject *, module)
+    _PY_ENSURE_STACK_FOR_FN1_B(128*1024, NULL, PyObject *, _curses_initscr_impl, PyObject *, module)
     WINDOW *win;
 
     if (curses_initscr_called) {
