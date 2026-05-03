97899e5225544744da67056240ad0f6e0dba4b7a
JonathanRoach <jonroach@icloud.com>
2026-05-03 07:42:10 +0100

    Currect setting of COROUTINE_HAVE_ALLOCA_H

--- Include/internal/pycore_cor_platform.h
+++ Include/internal/pycore_cor_platform.h
@@ -6,7 +6,9 @@
 
 #define _Cor_thread_local _Py_thread_local
 
-#define COROUTINE_HAVE_ALLOCA_H HAVE_ALLOCA_H
+#ifndef HAVE_ALLOCA_H
+ #define COROUTINE_HAVE_ALLOCA_H HAVE_ALLOCA_H
+#endif
 
 #define _Cor_Mutex PyMutex
 static inline int _Cor_Mutex_ctor(_Cor_Mutex *mut){ *mut = (PyMutex){0}; return 0;}
